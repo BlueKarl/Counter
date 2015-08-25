8be8376e14ec75b3782a7008c0eb2cd2dd3f9b8b
BlueKarl <karl@e.hunantv.com>
2015-08-25 09:18:40 +0800

    修改了config中redis的地址和启动项

--- log/config.py
+++ log/config.py
@@ -1,6 +1,6 @@
 import os
 
-REDIS_HOST = os.getenv('REDIS_HOST', 'redis-logcount.yg.hunantv.com')
+REDIS_HOST = os.getenv('REDIS_HOST', '10.222.46.215')
 REDIS_PORT = os.getenv('REDIS_PORT', 8889)
 #REDIS_HOST = os.getenv('REDIS_HOST', 'localhost')
 #REDIS_PORT = os.getenv('REDIS_PORT', 6379)
